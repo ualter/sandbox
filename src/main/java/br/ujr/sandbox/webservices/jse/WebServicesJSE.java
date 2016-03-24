38530bd32b82bb4dee2529efb4f9bb97e342def8
Ualter Azambuja Junior <ualter@users.noreply.github.com>
2016-03-24 09:44:01 -0300

    Create WebServicesJSE.java

---
+++ src/main/java/br/ujr/sandbox/webservices/jse/WebServicesJSE.java
@@ -0,0 +1,17 @@
+package webservices.jse;
+
+import javax.jws.WebService;
+import javax.xml.ws.Endpoint;
+
+@WebService
+public class WebServicesJSE {
+	
+	public static void main(String[] args) {
+		Endpoint.publish("http://localhost:8080/WebServicesJSE", new WebServicesJSE());
+	}
+
+	public double sum(int value1, int value2) {
+		return value1 + value2;
+	}
+
+}
