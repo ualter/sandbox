7cd3b2f2e758ae5e0606c63f0f8690fc5630f816
ualter.junior@gmail.com <ualter.junior@gmail.com>
2017-07-13 23:04:28 +0200

    .

---
+++ src/main/java/br/ujr/sandbox/springboot/SpringBootApp.java
@@ -0,0 +1,12 @@
+package br.ujr.sandbox.springboot;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
+
+@Configuration
+@EnableAutoConfiguration
+@RestController
+public class SpringBootApp {
+
+}
