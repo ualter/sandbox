64276ab63c9d1b569ba2fecee555ddeb4e5e8eb9
ualter.junior@gmail.com <ualter.junior@gmail.com>
2018-03-17 20:35:08 +0100

    .

---
+++ src/main/java/br/ujr/sandbox/lombok/Employee.java
@@ -0,0 +1,13 @@
+package br.ujr.sandbox.lombok;
+
+import lombok.Data;
+
+@Data
+public class Employee {
+	
+	private Long id;
+	private String name;
+	
+	
+
+}
---
+++ src/main/java/br/ujr/sandbox/lombok/Main.java
@@ -0,0 +1,10 @@
+package br.ujr.sandbox.lombok;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		Employee e = new Employee();
+		e.getName();
+	}
+
+}
